b2dd4d5759b9116825f1555cdd19ac7b4e470d52
Josef Pihrt <josef@pihrt.net>
2023-09-02 19:05:35 +0200

    Migrate to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,19 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
       </div>
     </header>
   );
 }
 
-export default function Home() {
+export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
