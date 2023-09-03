28e3ae9c4b440df02e8a30a9220d13093d7b6917
Josef Pihrt <josef@pihrt.net>
2023-09-03 13:29:17 +0200

    Add component MapyCzEmbeddedPath

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,7 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import MapyCzEmbeddedPath from '@site/src/components/MapyCzEmbeddedPath';
+
+export default {
+  ...MDXComponents,
+  MapyCzEmbeddedPath,
+};
