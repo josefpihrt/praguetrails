dcf29e0356c80ef435ee3b379beea44db1ac5c72
Josef Pihrt <josef@pihrt.net>
2023-10-05 22:32:16 +0200

    Revert "x"
    This reverts commit e398919abf55764749a8fc724666b272a965aefb.

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,11 +1,7 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import MapyCzEmbeddedPath from '@site/src/components/MapyCzEmbeddedPath';
-import DesktopOrMobileLink from '@site/src/components/DesktopOrMobileLink';
-import {Sizes} from '@site/src/components/DesktopOrMobileLink/index';
 
 export default {
   ...MDXComponents,
   MapyCzEmbeddedPath,
-  DesktopOrMobileLink,
-  Sizes,
 };
