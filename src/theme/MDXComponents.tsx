1f79074e59a776f7fcf208ed64db7ab060ebee3d
Josef Pihrt <josef@pihrt.net>
2023-10-16 21:30:35 +0200

    update

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,11 +1,7 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import MapyCzEmbeddedPath from '@site/src/components/MapyCzEmbeddedPath';
-import TrainLink from '@site/src/components/TrainLink';
-import {TrainStations} from '@site/src/components/TrainLink/index';
 
 export default {
   ...MDXComponents,
   MapyCzEmbeddedPath,
-  TrainLink,
-  TrainStations,
 };
