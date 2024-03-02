027c927aae3d0c05549946b70d00aa3cd044cbbf
Josef Pihrt <josef@pihrt.net>
2024-03-02 23:04:09 +0100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,8 +38,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/josefpihrt/praguetrails/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
