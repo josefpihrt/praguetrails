16430f2fd5506c250627f8716030759ac193da19
Josef Pihrt <josef@pihrt.net>
2024-03-06 12:02:04 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ function HomepageHeader() {
         <img width="70%" src="img/prague-trails-horizontal-logo.png" />
         {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
         <h1 className="hero__subtitle">{siteConfig.tagline}</h1>
+        <span>(formerly bohemiantrails.cz)</span>
         <hr></hr>
         <a href='introduction#trails'>
           <img src="img/trails-map.png" />
